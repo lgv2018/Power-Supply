3f4b67e28b73aba302bffd00e37fc13d5a9218a3
ritesh4009 <ritesh4009@gmail.com>
2019-05-26 17:51:39 +0200

    optimization

---
+++ include/config.h
@@ -0,0 +1,87 @@
+//Defination of I2C devices
+LiquidCrystal_I2C lcd(0x27,20,4);
+//Defination of card 1
+MCP47X6 Card1_dac_volt = MCP47X6(0X60);
+MCP47X6 Card1_dac_current =MCP47X6(0X61);
+MCP342X Card1_ADC =MCP342X(0x68);
+//Defination of card 2
+MCP47X6 Card2_dac_volt = MCP47X6(0x62);
+MCP47X6 Card2_dac_current =MCP47X6(0x63);
+MCP342X Card2_ADC =MCP342X(0x6F);
+int DAC_bits = 4095;
+int ADC_bits = 32767;
+
+static int16_t vout1_adc, vin1_adc, current1_adc, T1_adc;
+static int16_t vout2_adc, vin2_adc, current2_adc, T2_adc;
+int v1_set_raw, v2_set_raw, a1_set_raw, a2_set_raw;
+uint16_t v1_set, v2_set, a1_set, a2_set;
+float vout1 = 0.000, vin1 =0.000, current1 = 0.000, T1 =0.000;
+float vout2 = 0.000, vin2 =0.000, current2 = 0.000, T2 =0.000;
+uint8_t arrow[8] = {0x0, 0x04 ,0x06, 0x1f, 0x06, 0x04, 0x00, 0x00};
+uint8_t ON[8] = {0x18,0x1C,0x1E,0x1F,0x1E,0x1C,0x18,0x00};
+uint8_t OFF[8] = {0x00,0x1F,0x1F,0x1F,0x1F,0x1F,0x1F,0x00};
+float delta =0.10;
+float max_volt = 20.48;
+float min_volt = 0.00;
+float max_current = 2.048;
+float min_current = 0.000;
+
+//Variables for the menu encoder
+int counter = 0; 
+int page=1;
+int Ready=1;
+int submenu=0;
+int menu=0;
+int last_counter = 0;   
+int pushed = 0;
+int DL = 100; // Delay for action
+float inc_r = 0.5;
+int i;
+int j;
+int lastctr = 0;
+int max_cnt =10;
+int min_cnt =0;
+
+float set_current[2] = {0.1,0.1};   //Current settings variable
+float set_volt[2] = {5,5};    //Current settings variable
+
+//Variables for LCD
+String voltage_h = "Volt(H):";
+String voltage_L = "Volt(L):";
+String R1 = "R1=";
+String R2 = "R2=";
+String Back = "Back";
+
+//Pin assignments
+#define push 4
+#define encoderPinA 2
+#define encoderPinB 3
+
+long summary_delay =5000;
+long reset_timer;
+
+volatile unsigned int encoderPos = 0;  // a counter for the dial
+unsigned int lastReportedPos = 1;   // change management
+static boolean rotating = false;    // debounce management
+boolean A_set = false;            // interrupt service routine vars
+boolean B_set = false;            // interrupt service routine vars
+void doEncoderA();
+void doEncoderB();
+void Summaryscreen();
+
+const byte ROWS = 5;
+const byte COLUMNS =4;
+//Define symbols on keypad
+char hexakeys [ROWS][COLUMNS] = {
+  {'A','B','#','*'},
+  {'1','2','3','U'},
+  {'4','5','6','D'},
+  {'7','8','9','E'},
+  {'<','0','>','C'}
+};
+
+byte rowPins[ROWS] = {13,12,11,10,9}; //connect to row pinout of the keypad
+byte columnPins[COLUMNS] = {5,6,A0,A1}; //connect to column pinout of keypad
+//initialize an instance of class NewKeypad
+Keypad Customkeypad = Keypad(makeKeymap (hexakeys),rowPins,columnPins,ROWS, COLUMNS);
+int keypadEntry = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -5,89 +5,7 @@
 #include <MCP47X6.h>
 #include <MCP342X.h>
 #include <Keypad.h>
-
-
-//Defination of I2C devices
-LiquidCrystal_I2C lcd(0x27,20,4);
-//Defination of card 1
-MCP47X6 Card1_dac_volt = MCP47X6(0X60);
-MCP47X6 Card1_dac_current =MCP47X6(0X61);
-MCP342X Card1_ADC =MCP342X(0x68);
-//Defination of card 2
-MCP47X6 Card2_dac_volt = MCP47X6(0x62);
-MCP47X6 Card2_dac_current =MCP47X6(0x63);
-MCP342X Card2_ADC =MCP342X(0x6F);
-
-static int16_t vout1_adc, vin1_adc, current1_adc, T1_adc;
-static int16_t vout2_adc, vin2_adc, current2_adc, T2_adc;
-int v1_set_raw, v2_set_raw, a1_set_raw, a2_set_raw;
-uint16_t v1_set, v2_set, a1_set, a2_set;
-float vout1 = 0.000, vin1 =0.000, current1 = 0.000, T1 =0.000;
-float vout2 = 0.000, vin2 =0.000, current2 = 0.000, T2 =0.000;
-uint8_t arrow[8] = {0x0, 0x04 ,0x06, 0x1f, 0x06, 0x04, 0x00, 0x00};
-uint8_t ON[8] = {0x18,0x1C,0x1E,0x1F,0x1E,0x1C,0x18,0x00};
-uint8_t OFF[8] = {0x00,0x1F,0x1F,0x1F,0x1F,0x1F,0x1F,0x00};
-
-//Variables for the menu encoder
-int counter = 0; 
-int page=1;
-int Ready=1;
-int submenu=0;
-int menu=0;
-int last_counter = 0;   
-int pushed = 0;
-int DL = 500; // Delay for action
-float inc_r = 0.5;
-int i;
-int j;
-int lastctr = 0;
-int max_cnt =10;
-int min_cnt =0;
-
-float set_current[2] = {1,1};   //Current settings variable
-float set_volt[2] = {1,1};    //Current settings variable
-
-//Variables for LCD
-String voltage_h = "Volt(H):";
-String voltage_L = "Volt(L):";
-String R1 = "R1=";
-String R2 = "R2=";
-String Back = "Back";
-
-//Pin assignments
-#define push 4
-#define encoderPinA 2
-#define encoderPinB 3
-#define Menu 5
-
-long summary_delay =5000;
-long reset_timer;
-
-volatile unsigned int encoderPos = 0;  // a counter for the dial
-unsigned int lastReportedPos = 1;   // change management
-static boolean rotating = false;    // debounce management
-boolean A_set = false;            // interrupt service routine vars
-boolean B_set = false;            // interrupt service routine vars
-void doEncoderA();
-void doEncoderB();
-void Summaryscreen();
-
-const byte ROWS = 5;
-const byte COLUMNS =4;
-//Define symbols on keypad
-char hexakeys [ROWS][COLUMNS] = {
-  {'A','B','#','*'},
-  {'1','2','3','U'},
-  {'4','5','6','D'},
-  {'7','8','9','*'},
-  {'<','0','>','*'}
-};
-
-byte rowPins[ROWS] = {1,3,5,8,10}; //connect to row pinout of the keypad
-byte columnPins[COLUMNS] = {2,4,6,9}; //connect to column pinout of keypad
-//initialize an instance of class NewKeypad
-Keypad Customkeypad = Keypad(makeKeymap (hexakeys),rowPins,columnPins,ROWS, COLUMNS);
-int keypadEntry = 0;
+#include <config.h>
 
 void setup() 
 {
@@ -98,24 +16,22 @@ void setup()
   attachInterrupt(0, doEncoderA, CHANGE);  // encoder pin on interrupt 0 (pin 2)
   attachInterrupt(1, doEncoderB, CHANGE);  // encoder pin on interrupt 1 (pin 3)
   Serial.begin(9600);
-
-  //Define the pin as input & OUTPUT
-  pinMode (push,INPUT);
+  pinMode (push,INPUT); //Define the pin as input & OUTPUT
   
   // Read EEPROM settings
-  int EEPROM_add = 0;
+  /* int EEPROM_add = 0;
   for(i=0;i<2;i++)
   { 
     for (j=0;j<4;j++)
     {
-      /* R[i][j]=EEPROM.read(EEPROM_add);
+      R[i][j]=EEPROM.read(EEPROM_add);
       if (R[i][j] >20 || R[i][j] <0) R[i][j] = 10;
       v_cal[i][j]=EEPROM.read(EEPROM_add+10);
       if (v_cal[i][j] >20 || v_cal[i][j] <0) v_cal[i][j] = 10;
-      EEPROM_add++; */
+      EEPROM_add++;
     }
   }
-
+ */
   // Initialize LCD
   lcd.begin();
   lcd.backlight(); 
@@ -161,10 +77,10 @@ void setup()
                   MCP342X_SIZE_16BIT |
                   MCP342X_GAIN_1X);
 
-  Card1_dac_current.setOutputLevel(uint16_t (set_current[0]*4096/2.048));
-  Card1_dac_volt.setOutputLevel(uint16_t (set_volt[0]*4096/20.48));
-  Card2_dac_current.setOutputLevel(uint16_t (set_current[1]*4096/2.048));
-  Card2_dac_volt.setOutputLevel(uint16_t (set_volt[1]*4096/20.48));
+  Card1_dac_current.setOutputLevel(uint16_t (set_current[0]*DAC_bits/max_current));
+  Card1_dac_volt.setOutputLevel(uint16_t (set_volt[0]*DAC_bits/max_volt));
+  Card2_dac_current.setOutputLevel(uint16_t (set_current[1]*DAC_bits/max_current));
+  Card2_dac_volt.setOutputLevel(uint16_t (set_volt[1]*DAC_bits/max_volt));
   
 }
 void loop() {
@@ -177,12 +93,11 @@ void loop() {
 
   if((last_counter > counter) || (last_counter < counter)  || pushed) //Only print on the LCD when a step is detected or the button is pushed  
    {
-    menu=1;                                                                                                                                                                                                                           menu =1;
+    menu=1;                                                                                                                                                                                                                          menu =1;
     Ready=1;
     if(submenu == 0) //Main Menu
     { 
       reset_timer= millis ();
-      Serial.println(reset_timer);
       max_cnt=2;
       if(0 <= counter && counter < 1)
       {
@@ -194,6 +109,7 @@ void loop() {
         lcd.print(" Voltage settings");
         lcd.setCursor(1,2);
         lcd.print(Back);
+        pushed =0;
         page=1;
       }
       if(1 <= counter && counter < 2)
@@ -207,6 +123,7 @@ void loop() {
         lcd.print("Voltage settings");
         lcd.setCursor(1,2);
         lcd.print(Back);
+        pushed = 0;
         page=2;
       }
       if(2 <= counter && counter < 3)
@@ -219,6 +136,7 @@ void loop() {
         lcd.setCursor(0,2);
         lcd.write(1);
         lcd.print(Back);
+        pushed =0;
         page=3;
       }
     }//submenu = 0;
@@ -373,6 +291,7 @@ void loop() {
     { 
       counter = 0;
       lastctr = 0;
+      Serial.print(page);
        if(page==1 || page==2 )
        {
          int COL = 0;
@@ -392,25 +311,41 @@ void loop() {
          {
           if(counter != lastctr)
           {
-            if (counter > lastctr) set_current[COL] = set_current[COL]+0.01;
-            if (counter < lastctr) set_current[COL] = set_current[COL]-0.01;
-            if (set_current[COL]>2.048) set_current [COL]= 2.048;
-            if (set_current[COL]<0) set_current [COL] = 0.0;
+            if (counter > lastctr) set_current[COL] = set_current[COL]+delta;
+            if (counter < lastctr) set_current[COL] = set_current[COL]-delta;
+            if (set_current[COL]>max_current) set_current [COL]= max_current;
+            if (set_current[COL]<min_current) set_current [COL] =min_current;
           }
           lcd.setCursor(14,ROW);
           lcd.write(1);
+          //lcd.cursor();
+          //lcd.blink_on();
           switch (page)
           {
             case 1:
-              Card1_dac_current.setOutputLevel(uint16_t (set_current[COL]*4096/2.048));
+              Card1_dac_current.setOutputLevel(uint16_t (set_current[COL]*DAC_bits/max_current));
               break;
             case 2:
-              Card2_dac_current.setOutputLevel(uint16_t (set_current[COL]*4096/2.048));
+              Card2_dac_current.setOutputLevel(uint16_t (set_current[COL]*DAC_bits/max_current));
               break;
           }
           lcd.print(set_current[COL]);
           lastctr = counter;
-          delay(100);
+          delay(100); //important for debouncing of encoder push switch
+          char customkey1 =Customkeypad.getKey();
+          if(customkey1 == 'U') delta=delta*10;
+          if(customkey1 == 'D') delta=delta/10;
+          if(customkey1 == 'E')
+          {
+            last_counter = 0;
+            counter=0;
+            pushed=0;
+            Ready=0;
+            keypadEntry = 0;
+            submenu = 0;
+            menu =0;
+            goto Start;
+          }
           if(!digitalRead(push))
           {
             page = ROW +1;
@@ -454,33 +389,50 @@ void loop() {
          lcd.print(" ");
          do
          {
+          Serial.print(delta);
           if(counter != lastctr)
           {
-            if (counter > lastctr) set_volt[COL] = set_volt[COL]+0.1;
-            if (counter < lastctr) set_volt[COL] = set_volt[COL]-0.1;
-            if (set_volt[COL]>20.48) set_volt[COL]= 20.48;
-            if (set_volt[COL]<0) set_volt[COL] = 0.000;
+            if (counter > lastctr) set_volt[COL] = set_volt[COL]+delta;
+            if (counter < lastctr) set_volt[COL] = set_volt[COL]-delta;
+            if (set_volt[COL]>max_volt) set_volt[COL]= max_volt;
+            if (set_volt[COL]<min_volt) set_volt[COL] = min_volt;
           }
           lcd.setCursor(14,ROW);
           lcd.write(1);
           switch (page)
           {
             case 1:
-              Card1_dac_volt.setOutputLevel(uint16_t (set_volt[COL]*4096/20.48));
+              Card1_dac_volt.setOutputLevel(uint16_t (set_volt[COL]*DAC_bits/max_volt));
               break;
             case 2:
-              Card2_dac_volt.setOutputLevel(uint16_t (set_volt[COL]*4096/20.48));
+              Card2_dac_volt.setOutputLevel(uint16_t (set_volt[COL]*DAC_bits/max_volt));
               break;
           }
           lcd.print(set_volt[COL]);
           lastctr = counter;
-          delay(100);
+          delay(100); //important for debouncing of encoder push switch
+          char customkey1 =Customkeypad.getKey();
+          if(customkey1 == 'U') delta=delta*10;
+          if(customkey1 == 'D') delta=delta/10;
+          if(customkey1 == 'E')
+          {
+            last_counter = 0;
+            counter=0;
+            pushed=0;
+            Ready=0;
+            submenu =0;
+            keypadEntry = 0;
+            menu =0;
+            goto Start;
+          }
           if(!digitalRead(push))
           {
+            page = ROW +1;
             delay(DL);
             counter=0;
             pushed=0;
             Ready=0;
+            keypadEntry = 0;
             goto Start;
           }
          }while(1);
@@ -497,11 +449,11 @@ void loop() {
     }//end of submenu 2
   }
 
-    if (millis() - reset_timer > summary_delay)
+  if (millis() - reset_timer > summary_delay)
   {
     menu = 0;
   }
-  
+
   //Add limit for the counter. Each line of the menu has 2 points. Since my menu has 5 lines the maximum counter will be from 0 to 10
   if(counter > max_cnt) counter=0;
   if(counter < 0) counter=max_cnt;  
@@ -544,6 +496,7 @@ void Summaryscreen(){
         do {
         //Reading from ADC of Card1
         char customkey =Customkeypad.getKey();
+        //Serial.print(customkey);
         switch (customkey)
         {
         case '1': // change current setting of the card 1
@@ -566,11 +519,11 @@ void Summaryscreen(){
           return;
           break;
 
-        case '4': // change volt setting of the card 1
+        case '4': // change volt setting of the card 2
           menu = 1; // to prevent progam entering summary screen screen
           pushed = 1; //Entry in to menu
           submenu =2; //jump to submenu 2 for voltage setting
-          counter = 0; //Entry in page 1 of submenu 1
+          counter = 0; //Entry in page 1 of submenu 2
           page=1; //required for the pushed button function
           keypadEntry = 1; //forces entry for pushe button function
           return;
@@ -579,43 +532,43 @@ void Summaryscreen(){
         case '5': // change volt setting of the card 2
           menu = 1; // to prevent progam entering summary screen screen
           pushed = 1; //Entry in to menu
-          submenu =1; //jump to submenu 1
-          counter = 1; //Entry in page 2 of submenu 1
+          submenu =2; //jump to submenu 2 for voltage setting
+          counter = 1; //Entry in page 2 of submenu 2
           page=2; //required for the pushed button function
           keypadEntry = 1; //forces entry for pushe button function
           return;
           break;
-
+        
         default:
           break;
-        }
+        } 
         
         Card1_ADC.startConversion(MCP342X_CHANNEL_1);
         Card1_ADC.getResult(&vout1_adc);
-        vout1 = vout1_adc*20.480/32767;
+        vout1 = vout1_adc*max_volt/ADC_bits;
         Card1_ADC.startConversion(MCP342X_CHANNEL_2);
         Card1_ADC.getResult(&current1_adc);
-        current1 = current1_adc*2.048/32767;
+        current1 = current1_adc*max_current/ADC_bits;
         Card1_ADC.startConversion(MCP342X_CHANNEL_3);
         Card1_ADC.getResult(&vin1_adc);
-        vin1 = vin1_adc*20.480*4/32767;
+        vin1 = vin1_adc*max_volt*4/ADC_bits;
         Card1_ADC.startConversion(MCP342X_CHANNEL_4);
         Card1_ADC.getResult(&T1_adc);
-        T1 = ((T1_adc*2.048/32767)-0.5)*100;
+        T1 = ((T1_adc*max_current/ADC_bits)-0.5)*100;
 
         //Reading from ADC of Card2
         Card2_ADC.startConversion(MCP342X_CHANNEL_1);
         Card2_ADC.getResult(&vout2_adc);
-        vout2 = vout2_adc*20.480/32767;
+        vout2 = vout2_adc*max_volt/ADC_bits;
         Card2_ADC.startConversion(MCP342X_CHANNEL_2);
         Card2_ADC.getResult(&current2_adc);
-        current2 = current2_adc*2.048/32767;
+        current2 = current2_adc*max_current/ADC_bits;
         Card2_ADC.startConversion(MCP342X_CHANNEL_3);
         Card2_ADC.getResult(&vin2_adc);
-        vin2 = vin2_adc*20.480*4/32767;
+        vin2 = vin2_adc*max_volt*4/ADC_bits;
         Card2_ADC.startConversion(MCP342X_CHANNEL_4);
         Card2_ADC.getResult(&T2_adc);
-        T2 = ((T2_adc*2.048/32767)-0.5)*100;
+        T2 = ((T2_adc*max_current/ADC_bits)-0.5)*100;
         
         //Printing all data 
         lcd.setCursor(4,0);lcd.print(vout1,3);lcd.setCursor(14,0);lcd.print(current1,3);lcd.print(" ");
@@ -623,10 +576,11 @@ void Summaryscreen(){
         lcd.setCursor(4,2);lcd.print(vout2,3);lcd.setCursor(14,2);lcd.print(current2,3);lcd.print(" ");
         lcd.setCursor(4,3);lcd.print(vin2,2);lcd.setCursor(14,3);lcd.print(T2,1);lcd.print(" ");
       
-        if((last_counter > counter) || (last_counter < counter)  || pushed)
+        if((last_counter > counter) || (last_counter < counter))
         {
           last_counter = counter;
           counter = 0;
+          pushed = 1;
           menu = 1;
           return;
         }
